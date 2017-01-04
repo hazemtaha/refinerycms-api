431fa8694eb0bcb5aa2387b0edf386760abd524d
andrewkhouzam <andrew.khouzam@gmail.com>
2017-01-04 17:06:05 +0200

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,71 +1,77 @@
-$VERBOSE = ENV['VERBOSE'] || false
-
 require 'rubygems'
-
-ENGINE_RAILS_ROOT = File.join(File.dirname(__FILE__), '../') unless defined?(ENGINE_RAILS_ROOT)
 
 # Configure Rails Environment
 ENV["RAILS_ENV"] ||= 'test'
 
-if ENV['TRAVIS']
-  require 'coveralls'
-  Coveralls.wear!
-end
 
-require File.expand_path("../dummy/config/environment", __FILE__)
-
-require 'rspec/rails'
-require 'capybara/rspec'
-
-if ENV['RETRY_COUNT']
-  require 'rspec/retry'
-  RSpec.configure do |config|
-    # rspec-retry
-    config.verbose_retry = true
-    config.default_sleep_interval = 0.33
-    config.clear_lets_on_failure = true
-    config.default_retry_count = ENV["RETRY_COUNT"]
+if ENV["COVERAGE"]
+  # Run Coverage report
+  require 'simplecov'
+  SimpleCov.start do
+    add_group 'Controllers', 'app/controllers'
+    add_group 'Helpers', 'app/helpers'
+    add_group 'Mailers', 'app/mailers'
+    add_group 'Models', 'app/models'
+    add_group 'Views', 'app/views'
+    add_group 'Libraries', 'lib'
   end
 end
 
-Rails.backtrace_cleaner.remove_silencers!
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= 'test'
+
+begin
+  require File.expand_path("../dummy/config/environment", __FILE__)
+rescue LoadError
+  puts "Could not load dummy application. Please ensure you have run `bundle exec rake test_app`"
+  exit
+end
+
+require 'rspec/rails'
+# require 'ffaker'
+require 'capybara/rspec'
+
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|f| require f}
+
+# require 'refinery/testing_support/factories'
+# require 'refinery/testing_support/preferences'
+
+require 'refinery/api/testing_support/caching'
+require 'refinery/api/testing_support/helpers'
+require 'refinery/api/testing_support/setup'
+# require 'refinery/testing_support/shoulda_matcher_configuration'
 
 RSpec.configure do |config|
   config.mock_with :rspec
   config.filter_run :focus => true
-  config.filter_run :js => true if ENV['JS'] == 'true'
-  config.filter_run :js => nil if ENV['JS'] == 'false'
   config.run_all_when_everything_filtered = true
-  config.include ActionView::TestCase::Behavior, :file_path => %r{spec/presenters}
+  config.backtrace_exclusion_patterns = %w(
+    rails actionpack railties capybara activesupport rack warden rspec actionview
+    activerecord dragonfly benchmark
+  ).map { |noisy| /#{noisy}/ }
+  config.color = true
+  config.fail_fast = ENV['FAIL_FAST'] || false
   config.infer_spec_type_from_file_location!
+  config.raise_errors_for_deprecations!
+  config.use_transactional_fixtures = true
 
-  config.before(:each) do
-    ::I18n.default_locale = I18n.locale = Globalize.locale = :en
+  config.include FactoryGirl::Syntax::Methods
+  config.include Refinery::Api::TestingSupport::Helpers, :type => :controller
+  config.extend Refinery::Api::TestingSupport::Setup, :type => :controller
+  # config.include Refinery::TestingSupport::Preferences, :type => :controller
+
+  config.before do
+    Refinery::Api.requires_authentication = true
   end
-
-  unless ENV['FULL_BACKTRACE']
-    config.backtrace_exclusion_patterns = %w(
-      rails actionpack railties capybara activesupport rack warden rspec actionview
-      activerecord dragonfly benchmark quiet_assets rubygems
-    ).map { |noisy| /\b#{noisy}\b/ }
-  end
-
-  # Store last errors so we can run rspec with --only-failures
-  config.example_status_persistence_file_path = ".rspec_failures"
 end
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories including factories.
-([ENGINE_RAILS_ROOT, Rails.root.to_s].uniq | Refinery::Plugins.registered.pathnames).map{ |p|
+([Rails.root.to_s] | ::Refinery::Plugins.registered.pathnames).map{|p|
   Dir[File.join(p, 'spec', 'support', '**', '*.rb').to_s]
 }.flatten.sort.each do |support_file|
   require support_file
 end
-
-Capybara.register_driver :poltergeist_debug do |app|
-  Capybara::Poltergeist::Driver.new(app, debug: false, js_errors:  true, inspector: :open)
-end
-
-require 'capybara/poltergeist'
-Capybara.javascript_driver = :poltergeist
-Capybara.always_include_port = true
