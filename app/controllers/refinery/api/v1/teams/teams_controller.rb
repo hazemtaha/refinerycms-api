d2314f512995947de6304204e08e4f04afbb48b2
Hazem Taha <thatskan@gmail.com>
2017-01-10 16:04:00 +0200

    add update teams controller

--- app/controllers/refinery/api/v1/teams/teams_controller.rb
+++ app/controllers/refinery/api/v1/teams/teams_controller.rb
@@ -1,77 +1,80 @@
-module Refinery
-  module Api
-    module V1
-      class TeamsController < Refinery::Api::BaseController
+if defined?(Refinery::Teams)
+  module Refinery
+    module Api
+      module V1
+        module Teams
+          class TeamsController < Refinery::Api::BaseController
 
-        def index
-          if params[:ids]
-            @teams = Refinery::Teams::Team.
-                      includes(:photo).
-                      accessible_by(current_ability, :read).
-                      where(id: params[:ids].split(','))
-          else
-            @teams = Refinery::Teams::Team.
-                      includes(:photo).
-                      accessible_by(current_ability, :read).
-                      # ransack(params[:q]).result
-                      # order(:lft)
-                      order("created_at DESC")
+            def index
+              if params[:ids]
+                @teams = Refinery::Teams::Team.
+                          includes(:photo).
+                          accessible_by(current_ability, :read).
+                          where(id: params[:ids].split(','))
+              else
+                @teams = Refinery::Teams::Team.
+                          includes(:photo).
+                          accessible_by(current_ability, :read).
+                          # ransack(params[:q]).result
+                          # order(:lft)
+                          order("created_at DESC")
+              end
+              respond_with(@teams)
+            end
+
+            # def show
+            #   @page = page
+            #   respond_with(@page)
+            # end
+            #
+            # def new
+            # end
+            #
+            # def create
+            #   authorize! :create, Page
+            #   @page = Refinery::Page.new(page_params)
+            #
+            #   if @page.save
+            #     respond_with(@page, status: 201, default_template: :show)
+            #   else
+            #     invalid_resource!(@page)
+            #   end
+            # end
+            #
+            # def update
+            #   authorize! :update, page
+            #   if page.update_attributes(page_params)
+            #     respond_with(page, status: 200, default_template: :show)
+            #   else
+            #     invalid_resource!(page)
+            #   end
+            # end
+            #
+            # def destroy
+            #   authorize! :destroy, page
+            #   page.destroy
+            #   respond_with(page, status: 204)
+            # end
+            #
+            # private
+            #
+            # def page
+            #   @page ||= Refinery::Page.
+            #               includes(:translations, :parts).
+            #               accessible_by(current_ability, :read).
+            #               find(params[:id])
+            # end
+            #
+            # def page_params
+            #   if params[:page] && !params[:page].empty?
+            #     params.require(:page).permit(permitted_page_attributes)
+            #   else
+            #     {}
+            #   end
+            # end
+
           end
-
-          respond_with(@teams)
         end
-
-        # def show
-        #   @page = page
-        #   respond_with(@page)
-        # end
-        #
-        # def new
-        # end
-        #
-        # def create
-        #   authorize! :create, Page
-        #   @page = Refinery::Page.new(page_params)
-        #
-        #   if @page.save
-        #     respond_with(@page, status: 201, default_template: :show)
-        #   else
-        #     invalid_resource!(@page)
-        #   end
-        # end
-        #
-        # def update
-        #   authorize! :update, page
-        #   if page.update_attributes(page_params)
-        #     respond_with(page, status: 200, default_template: :show)
-        #   else
-        #     invalid_resource!(page)
-        #   end
-        # end
-        #
-        # def destroy
-        #   authorize! :destroy, page
-        #   page.destroy
-        #   respond_with(page, status: 204)
-        # end
-        #
-        # private
-        #
-        # def page
-        #   @page ||= Refinery::Page.
-        #               includes(:translations, :parts).
-        #               accessible_by(current_ability, :read).
-        #               find(params[:id])
-        # end
-        #
-        # def page_params
-        #   if params[:page] && !params[:page].empty?
-        #     params.require(:page).permit(permitted_page_attributes)
-        #   else
-        #     {}
-        #   end
-        # end
-
       end
     end
   end
